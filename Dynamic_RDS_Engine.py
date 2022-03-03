a3ca9781df7a0b7b35fabdaf286d2e7e06dae721
ShadowLight8 <stuff@nickandbeth.com>
2022-03-02 23:30:13 -0500

    Moved transmitRDS to be part of QN80xx. Cleaned up basicI2C usage. Changed Verbose logging to Excessive to better match FPP logging level and be a level more detailed that debug. Removed old code and comments.

--- Dynamic_RDS_Engine.py
+++ Dynamic_RDS_Engine.py
@@ -11,13 +11,55 @@ import smbus
 from time import sleep
 from datetime import datetime
 
-#@atexit.register
-#def cleanup():
-#  try:
-#    logging.debug('Cleaning up fifo')
-#    os.unlink(fifo_path)
-#  except:
-#    pass
+@atexit.register
+def cleanup():
+  try:
+    logging.debug('Cleaning up fifo')
+    os.unlink(fifo_path)
+  except:
+    pass
+
+# ===============
+# Basic I2C Class
+# ===============
+# Used by the Transmitter child classes (if they are i2c), but could also be used on its own if needed
+# TODO: Assuming SMBus of 1 on most modern hardware - Test what happens if this fails to setup a fall back to SMBus(0), maybe a config option?
+class basicI2C(object):
+  def __init__(self, address, bus=1):
+    self.address = address
+    self.bus = smbus.SMBus(bus)
+
+  def write(self, address, values):
+    # Simple i2c write - Always takes an list, even for 1 byte
+    logging.excessive('I2C write at 0x{0:02x} of {1}'.format(address, ' '.join('0x{:02x}'.format(a) for a in values)))
+    for _ in range(3):
+      try:
+        self.bus.write_i2c_block_data(self.address, address, values)
+      except Exception:
+        logging.exception("write_i2c_block_data error")
+        continue
+      else:
+        break
+    else:
+      logging.error("failed to write after 3 attempts")
+      # TODO: What is we get here?
+      #exit(-1)
+
+  def read(self, address, num_bytes):
+    # Simple i2c read - Always returns a list
+    for _ in range(3):
+      try:
+        retVal = self.bus.read_i2c_block_data(self.address, address, num_bytes)
+        logging.excessive('I2C read at 0x{0:02x} of {1} byte(s) returned {2}'.format(address, num_bytes, ' '.join('0x{:02x}'.format(a) for a in retVal)))
+        return retVal
+      except Exception:
+        logging.exception("read_i2c_block_data error")
+        continue
+      else:
+        break
+    else:
+      logging.error("failed to read after 3 attempts")
+      #exit(-1)
 
 # ===================
 # Transmitter Classes
@@ -25,6 +67,13 @@ from datetime import datetime
 # Generic representation of a Transmitter with a common interface
 # Includes a common RDSBuffer class
 # Specific implementations of both are expected by child classes
+
+# Transmitter
+#   RDSBuffer
+#
+# QN80xx (Transmitter)
+#   PSBuffer (RDSBuffer)
+#   RTBuffer (RDSBuffer)
 
 class Transmitter:
   def __init__(self):
@@ -64,27 +113,25 @@ class Transmitter:
     #logging.debug('Transmitter sendNextRDSGroup')
     pass
 
-  # TODO: Maybe keep RDS to specific impls since you could control other transmitters with this plugin that don't have RDS - Nah, parent class should have core impl and child class can override with a do nothing method
-
-  # ===============================================
-  # RDS Buffer Classes (Inner class of Transmitter)
-  # ===============================================
+  # =============================================
+  # RDS Buffer Class (Inner class of Transmitter)
+  # =============================================
   # This holds a string of RDS data to send, how much can be displayed at a time, how many chars per RDS group, and how long between updates
   # Typically two are created by a specific transmitter, one for sending out the PS groups and one for the RT groups
   # Data - Entire string to show on RDS Screen over time - updateData called once per track, resets all counters
-  # Fragment - What's on a single RDS Screen - Hold 8 for PS or 32/64 chars for RT - sendNextGroup tracks time to determine when to move to next fragment
+  # Fragment - What's on a single RDS Screen - Holds 8 for PS or 32/64 chars for RT - sendNextGroup tracks time to determine when to move to next fragment
   # Group - Single RDS Data Packet - Holds 2 or 4 chars - sendNextGroup called multiple times per second
 
   class RDSBuffer:
     def __init__(self, data='', frag_size=0, group_size=0, delay=4):
-      logging.debug('RDSBuffer __init__')
+      #logging.debug('RDSBuffer __init__')
       self.frag_size = frag_size
       self.group_size = group_size
       self.delay = delay
       self.updateData(data)
 
     def updateData(self, data):
-      logging.debug('RDSBuffer updateData')
+      #logging.debug('RDSBuffer updateData')
       self.fragments = []
       self.currentFragment = 0
       self.lastFragmentTime = datetime.now()
@@ -92,7 +139,7 @@ class Transmitter:
       for i in range(0, len(data), self.frag_size):
         self.fragments.append(data[i : i + self.frag_size])
       # TODO: Improve how we cut up the data (smart split) - Align with spaces, etc
-      #       In context of an FPP Plugin, have main script function deal with this and keep transmitter impl simple
+      #       In context of an FPP Plugin, have main script function deal with this and keep transmitter impl simple - Main function already has to put together the data from FPP
 
     def sendNextGroup(self):
       # Must be defined by child class
@@ -102,9 +149,20 @@ class Transmitter:
 class QN80xx(Transmitter):
   def __init__(self):
     super().__init__()
-    #logging.debug('QN80xx __init__')
-    self.PS = self.PSBuffer(' ', 4)
-    self.RT = self.RTBuffer(' ', 7)
+    self.I2C = basicI2C(0x21)
+    self.PS = self.PSBuffer(self, ' ', 4)
+    self.RT = self.RTBuffer(self, ' ', 7)
+
+  def startup(self):
+    # TODO: Next up is to build out what it takes to fully initialize the QN8066
+    try:
+      if not (self.I2C.read(0x01, 1)[0]>>6 & 1):
+        self.I2C.write(0x01, [0x41])
+        sleep(0.5)
+    except Exception:
+      logging.error('Failed to initialize transmitter')
+      exit(-1)
+    super().startup()
 
   def updateRDSData(self, PSdata, RTdata):
     #super().updateRDSData(data) - Not sure if this will be needed
@@ -119,11 +177,36 @@ class QN80xx(Transmitter):
     self.PS.sendNextGroup()
     self.RT.sendNextGroup()
 
+  def MOCK_transmitRDS(self, rdsBytes):
+    # Useful for testing
+    logging.excessive('MOCK Transmit {0}'.format(' '.join('0x{:02x}'.format(a) for a in rdsBytes)))
+    sleep(0.0876)
+
+  def transmitRDS(self, rdsBytes):
+    # Specific to QN 8036 and 8066 chips
+    rdsStatusByte = self.I2C.read(0x01, 1)[0]
+    rdsSendToggleBit = rdsStatusByte >> 1 & 0b1
+    rdsSentStatusToggleBit = self.I2C.read(0x1a, 1)[0] >> 2 & 0b1
+    logging.excessive('Transmit {0} - Send Bit {1} - Status Bit {2}'.format(' '.join('0x{:02x}'.format(a) for a in rdsBytes), rdsSendToggleBit, rdsSentStatusToggleBit))
+    self.I2C.write(0x1c, rdsBytes)
+    self.I2C.write(0x01, [rdsStatusByte ^ 0b10])
+    # RDS specifications indicate 87.6ms to send a group
+    # sleep is a bit less, plus time to read the status toggle bit
+    # In testing, loop is only executed once about every 30 seconds
+    sleep(0.0865)
+    while (self.I2C.read(0x1a, 1)[0] >> 2 & 1) == rdsSentStatusToggleBit:
+      logging.excessive('Waiting for rdsSentStatusToggleBit to flip')
+      sleep(0.001)
+      # TODO: If we hit this more than a few times, something is wrong....how to reset? Maybe a max number of tries, then move on?
+
   class PSBuffer(Transmitter.RDSBuffer):
     # Sends RDS type 0B groups - Program Service
     # Fragment size of 8, Groups send 2 characters at a time
-    def __init__(self, data, delay=4):
+    def __init__(self, outer, data, delay=4):
       super().__init__(data, 8, 2, delay)
+      # Include outer for the common transmitRDS function that both PSBugger and RTBuffer use
+      # TODO: Not sure if this is the best way yet
+      self.outer = outer
       #logging.debug('PSBuffer __init__')
 
     def updateData(self, data):
@@ -131,15 +214,15 @@ class QN80xx(Transmitter):
       # Adjust last fragment to make all 8 characters long
       #logging.debug('PSBuffer updateData')
       self.fragments[-1] = self.fragments[-1].ljust(self.frag_size)
-      logging.info('Updated PS Data {}'.format(self.fragments))
-      logging.info('PS Fragment \'{}\''.format(self.fragments[self.currentFragment]))
+      logging.info('Updated PS Fragments {}'.format(self.fragments))
+      #logging.info('Current PS Fragment \'{}\''.format(self.fragments[self.currentFragment]))
 
     def sendNextGroup(self):
       #logging.debug('PSBuffer sendNextGroup')
       if self.currentGroup == 0 and (datetime.now() - self.lastFragmentTime).total_seconds() >= self.delay:
         self.currentFragment = (self.currentFragment + 1) % len(self.fragments)
         self.lastFragmentTime = datetime.now()
-        logging.info('PS Fragment \'{}\''.format(self.fragments[self.currentFragment]))
+        logging.info('Send PS Fragment \'{}\''.format(self.fragments[self.currentFragment]))
 
       # TODO: Seems like this could be improved
       rdsBytes = [pi_byte1, pi_byte2, 0b10<<2 | pty>>3, (0b00111 & pty)<<5 | self.currentGroup, pi_byte1, pi_byte2]
@@ -148,15 +231,16 @@ class QN80xx(Transmitter):
 
       # Will block for ~87.6ms for RDS Group to be sent
       #MOCK_transmitRDS([pi_byte1, pi_byte2, 0b10<<2 | pty>>3, (0b00111 & pty)<<5 | self.currentGroup, pi_byte1, pi_byte2, ord(char1), ord(char2)])
-      transmitRDS(rdsBytes)
+      self.outer.transmitRDS(rdsBytes)
       self.currentGroup = (self.currentGroup + 1) % (self.frag_size // self.group_size)
 
   class RTBuffer(Transmitter.RDSBuffer):
     # Sends RDS type 2A groups - RadioText
     # Max fragment size of 64, Groups send 4 characters at a time
-    def __init__(self, data, delay=7):
+    def __init__(self, outer, data, delay=7):
       self.ab = 0
       super().__init__(data, 32, 4, delay)
+      self.outer = outer
 
     def updateData(self, data):
       super().updateData(data)
@@ -164,8 +248,8 @@ class QN80xx(Transmitter):
       if len(self.fragments[-1]) < self.frag_size:
         self.fragments[-1] += chr(0x0d)
       self.ab = not self.ab
-      logging.info('Updated RT Data {}'.format(self.fragments))
-      logging.info('RT Fragment \'{}\''.format(self.fragments[self.currentFragment].replace('\r','\\r')))
+      logging.info('Updated RT Fragments {}'.format(self.fragments))
+      #logging.info('RT Fragment \'{}\''.format(self.fragments[self.currentFragment].replace('\r','\\r')))
 
     def sendNextGroup(self):
       # Will block for ~80-90ms for RDS Group to be sent
@@ -176,7 +260,7 @@ class QN80xx(Transmitter):
         self.currentFragment = (self.currentFragment + 1) % len(self.fragments)
         self.lastFragmentTime = datetime.now()
         self.ab = not self.ab
-        logging.info('RT Fragment \'{}\''.format(self.fragments[self.currentFragment].replace('\r','\\r')))
+        logging.info('Send RT Fragment \'{}\''.format(self.fragments[self.currentFragment].replace('\r','\\r')))
 
       # TODO: Seems like this could be improved
       #RDS_RT = [pi_byte1, pi_byte2, gtype<<4 | b0<<3 | tp<<2 | pty>>3, ((pty & 4) + (pty & 2) + (pty & 1))<<5 | ab<<4 | c<<3, ord('I'), ord(' '), ord('g'), ord('o')]
@@ -188,11 +272,15 @@ class QN80xx(Transmitter):
 
       # Will block for ~87.6ms for RDS Group to be sent
       #MOCK_transmitRDS([pi_byte1, pi_byte2, 0b1000<<2 | pty>>3, (0b00111 & pty)<<5 | self.ab<<4 | self.currentGroup, ord(char1), ord(char2), ord(char3), ord(char4)])
-      transmitRDS(rdsBytes)
+      self.outer.transmitRDS(rdsBytes)
 
       self.currentGroup += 1
       if self.currentGroup * self.group_size >= len(self.fragments[self.currentFragment]):
         self.currentGroup = 0
+
+# ==================================
+# Configuration defaults and loading
+# ==================================
 
 # TODO for PLUGIN: Clean up default values
 # TODO for PLUGIN: Make sure all defaults support working out of the box
@@ -233,85 +321,15 @@ def read_config():
 	                	config[key] = val.replace('"', '').strip()
 	except IOError:
 		logging.warning('No config file found, using defaults.')
-	#logging.getLogger().setLevel(config['LoggingLevel'])
+	logging.getLogger().setLevel(config['LoggingLevel'])
 	logging.info('Config %s', config)
 
-class basicI2C(object):
-  def __init__(self, address):
-    self.address = address
-    # TODO: Assuming SMBus of 1 on most modern hardware - Test what happens if this fails to setup a fall back to SMBus(0), maybe a config option?
-    self.bus = smbus.SMBus(1)
-
-  def write(self, address, values):
-    # Simple i2c write - Always takes an list, even for 1 byte
-    logging.debug('I2C write at 0x{0:02x} of {1}'.format(address, ' '.join('0x{:02x}'.format(a) for a in values)))
-    for _ in range(3):
-      try:
-        self.bus.write_i2c_block_data(self.address, address, values)
-      except Exception:
-        logging.exception("write_i2c_block_data error")
-        continue
-      else:
-        break
-    else:
-      logging.error("failed to write after 3 attempts")
-      exit(-1)
-
-  def read(self, address, num_bytes):
-    # Simple i2c read - Always returns a list
-    for _ in range(3):
-      try:
-        retVal = self.bus.read_i2c_block_data(self.address, address, num_bytes)
-        logging.debug('I2C read at 0x{0:02x} of {1} byte(s) returned {2}'.format(address, num_bytes, ' '.join('0x{:02x}'.format(a) for a in retVal)))
-        return retVal
-      except Exception:
-        logging.exception("read_i2c_block_data error")
-        continue
-      else:
-        break
-    else:
-      logging.error("failed to read after 3 attempts")
-      exit(-1)
-
-# ======================
-# RDS Transmit Functions
-# ======================
-
-def MOCK_transmitRDS(rdsBytes):
-  # Useful for testing
-  logging.verbose('MOCK Transmit {0}'.format(' '.join('0x{:02x}'.format(a) for a in rdsBytes)))
-  sleep(0.0876)
-
-def transmitRDS(rdsBytes):
-  # Specific to QN 8036 and 8066 chips
-  rdsStatusByte = transmitter_I2C.read(0x01, 1)[0]
-  rdsSendToggleBit = rdsStatusByte >> 1 & 0b1
-  rdsSentStatusToggleBit = transmitter_I2C.read(0x1a, 1)[0] >> 2 & 0b1
-  logging.verbose('Transmit {0} - Send Bit {1} - Status Bit {2}'.format(' '.join('0x{:02x}'.format(a) for a in rdsBytes), rdsSendToggleBit, rdsSentStatusToggleBit))
-  transmitter_I2C.write(0x1c, rdsBytes)
-  transmitter_I2C.write(0x01, [rdsStatusByte ^ 0b10])
-  # RDS specifications indicate 87.6ms to send a group
-  # sleep is a bit less, plus time to read the status toggle bit
-  # In testing, loop is only executed once about every 30 seconds
-  sleep(0.0865)
-  while (transmitter_I2C.read(0x1a, 1)[0] >> 2 & 1) == rdsSentStatusToggleBit:
-    logging.verbose('Waiting for rdsSentStatusToggleBit to flip')
-    sleep(0.001)
-    # TODO: If we hit this more than a few times, something is wrong....how to reset? Maybe a max number of tries, then move on?
-
-def QN8066_init():
-  # TODO: Expand to all the QN8066 init work
-  # Start RDS
-
-  try:
-    if not (transmitter_I2C.read(0x01, 1)[0]>>6 & 1):
-      transmitter_I2C.write(0x01, [0x41])
-      sleep(0.5)
-  except Exception:
-    logging.error('Failed to initialize transmitter')
-    exit(-1)
+# ===============================
+# Processing FPP Data to RDS Data
+# ===============================
 
 def updateRDSData():
+	# Take the data from FPP and the configuration to build the actual RDS string
 	# TODO: Maybe provide an option between strict split and smart split
 	logging.info('Updating RDS Data')
 	logging.debug('Title %s', title)
@@ -345,12 +363,10 @@ def updateRDSData():
 		a=tmp_RDSTextArtist, aw=nearest(tmp_RDSTextArtist,32), \
 		n=tmp_RDSTextTrackNum, nw=nearest(tmp_RDSTextTrackNum, 32))
 
-	logging.info('Updated PS Text [%s]', PSstr)
-	logging.info('Updated RDS Text [%s]', RTstr)
+	logging.info('Updated PS Data [%s]', PSstr)
+	logging.info('Updated RT Data [%s]', RTstr)
 
 	transmitter.updateRDSData(PSstr, RTstr)
-	# PS.updateData(PSstr)
-	# RT.updateData(RTstr)
 
 def nearest(str, size):
 	# -(-X // Y) functions as ceiling division
@@ -365,21 +381,19 @@ script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 logging.basicConfig(filename=script_dir + '/Dynamic_RDS_Engine.log', level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 
-# Adding in verbose log level between debug and info
-# Allow for debug to be really detailed
-# Verbose is as deep as most people would want
-VERBOSE = 15
+# Adding in excessive log level below debug for very noisy items
+# Allow for debug to be reasonable
+# Debug is as deep as most people would want
+EXCESSIVE = 5
 
-def verbose(msg, *args, **kwargs):
-  if logging.getLogger().isEnabledFor(VERBOSE):
-    logging.log(VERBOSE, msg, *args, **kwargs)
+def excessive(msg, *args, **kwargs):
+  if logging.getLogger().isEnabledFor(EXCESSIVE):
+    logging.log(EXCESSIVE, msg, *args, **kwargs)
 
-logging.addLevelName(15, 'VERBOSE')
-logging.VERBOSE = VERBOSE
-logging.verbose = verbose
-logging.Logger.verbose = verbose
-
-logging.getLogger().setLevel(logging.INFO);
+logging.addLevelName(15, 'EXCESSIVE')
+logging.EXCESSIVE = EXCESSIVE
+logging.excessive = excessive
+logging.Logger.excessive = excessive
 
 logging.info('--------');
 
@@ -419,24 +433,7 @@ pi_byte1 = 0x81
 pi_byte2 = 0x9b
 pty = 0b00010
 
-# Setup transmitter
-# TODO: This will eventually be more like
-# Transmitter parent class
-#   QN8066 child class
-#   Si4713 child class
-# Transmitter interface
-#   setup - Initialization of transmitter
-#   reset - Back to an initial state; Si4713 is done with a reset pin; QN8066 by I2C write
-#   PSBuffer 
-#   RTBuffer
-#   sendNextGroup
-
 # TODO: Based on config init the correct transmitter
-
-#PS = PSBuffer(' ', 4)
-#RT = RTBuffer(' ', 7)
-transmitter_I2C = basicI2C(0x21)
-QN8066_init()
 transmitter = QN80xx()
 
 # =========
@@ -509,14 +506,12 @@ with open(fifo_path, 'r') as fifo:
 				# TANL is always sent together with L being last item, so we only need to update the RDS Data once with the new values
 				updateRDSData()
 				# TODO: Maybe updateRDSData should be part of the transmitter class? Not sure yet
-				# TODO: Check radio status between each track
+				transmitter.status()
 
 			else:
 				logging.error('Unknown fifo input %s', line)
 
 		else:
 			transmitter.sendNextRDSGroup()
-			#PS.sendNextGroup()
-			#RT.sendNextGroup()
 			# TODO: Determine when track length is done to reset RDS
 			# TODO: Could add 1 sec to length, so normally track change will update data rather than time expiring. Reset should only happen when playlist is stopped?
