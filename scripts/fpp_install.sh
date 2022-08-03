223f6ae828e10c89c49474397caf0c3c1bff7e03
ShadowLight8 <stuff@nickandbeth.com>
2022-08-02 21:30:06 -0400

    Added workaround to create python -> python3 symlink. Part of Issue #5

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -3,3 +3,11 @@
 sudo apt-get install -y python3-smbus
 
 #TODO: Disable onboard audio to use pwm hardware
+
+#Workaround for missing symlink of python -> python3 on BBB
+if [ -f /usr/bin/python3 ]; then
+  if [ ! -f /usr/bin/python ]; then
+    echo "Adding /usr/bin/python -> python3 symlink"
+    sudo ln -s python3 /usr/bin/python
+  fi
+fi
