4d43261d1ae28bbfd5dd7320405f9138adccdcf3
ShadowLight8 <stuff@nickandbeth.com>
2024-01-14 10:24:11 -0500

    Commenting out RPIO code for now. Getting ready to merge to enable PWM1 pin support

--- scripts/fpp_uninstall.sh
+++ scripts/fpp_uninstall.sh
@@ -2,8 +2,8 @@
 
 sudo /home/fpp/media/plugins/Dynamic_RDS/callbacks.py --exit
 
-echo Uninstalling RPIO...
-sudo pip uninstall -y RPIO
+#echo Uninstalling RPIO...
+#sudo pip uninstall -y RPIO
 
 echo "You can manually uninstall python3-smbus if nothing else uses it."
 echo "Command is: sudo apt-get remove -y python3-smbus"
