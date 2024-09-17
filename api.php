ec51d46c5d17fc686105979a7989aacd5fc7411d
ShadowLight8 <stuff@nickandbeth.com>
2024-09-16 21:40:04 -0400

    Update api.php to modify /boot/firmware/config.txt

--- api.php
+++ api.php
@@ -20,32 +20,32 @@ function DynRDSPiBootChange() {
     switch ($settingName) {
         case 'DynRDSAdvPISoftwareI2C':
            if (strcmp($myPluginSettings[$settingName],'1') == 0) {
-              exec("sudo sed -i -e 's/^dtparam=i2c_arm=on/#dtparam=i2c_arm=on/' /boot/config.txt");
-              exec("sudo sed -i -e '/^#dtparam=i2c_arm=on/a dtoverlay=i2c-gpio,i2c_gpio_sda=2,i2c_gpio_scl=3,i2c_gpio_delay_us=4,bus=1' /boot/config.txt");
+              exec("sudo sed -i -e 's/^dtparam=i2c_arm=on/#dtparam=i2c_arm=on/' /boot/firmware/config.txt");
+              exec("sudo sed -i -e '/^#dtparam=i2c_arm=on/a dtoverlay=i2c-gpio,i2c_gpio_sda=2,i2c_gpio_scl=3,i2c_gpio_delay_us=4,bus=1' /boot/firmware/config.txt");
            } else {
-              exec("sudo sed -i -e '/^dtoverlay=i2c-gpio,i2c_gpio_sda=2,i2c_gpio_scl=3,i2c_gpio_delay_us=4,bus=1/d' /boot/config.txt");
-              exec("sudo sed -i -e 's/^#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/config.txt");
+              exec("sudo sed -i -e '/^dtoverlay=i2c-gpio,i2c_gpio_sda=2,i2c_gpio_scl=3,i2c_gpio_delay_us=4,bus=1/d' /boot/firmware/config.txt");
+              exec("sudo sed -i -e 's/^#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/firmware/config.txt");
            }
            break;
 
         case 'DynRDSQN8066PIPWM':
            if (strcmp($myPluginSettings[$settingName],'1') == 0) {
-              exec("sudo sed -i -e 's/^dtparam=audio=on/#dtparam=audio=on/' /boot/config.txt");
+              exec("sudo sed -i -e 's/^dtparam=audio=on/#dtparam=audio=on/' /boot/firmware/config.txt");
               if (is_numeric(strpos($myPluginSettings['DynRDSAdvPIPWMPin'], ','))) {
-                exec("sudo sed -i -e '/^#dtparam=audio=on/a dtoverlay=pwm,pin=" . str_replace(",", ",func=", $myPluginSettings['DynRDSAdvPIPWMPin']) . "' /boot/config.txt");
+                exec("sudo sed -i -e '/^#dtparam=audio=on/a dtoverlay=pwm,pin=" . str_replace(",", ",func=", $myPluginSettings['DynRDSAdvPIPWMPin']) . "' /boot/firmware/config.txt");
               }
            } else {
-              exec("sudo sed -i -e '/^dtoverlay=pwm/d' /boot/config.txt");
-              exec("sudo sed -i -e 's/^#dtparam=audio=on/dtparam=audio=on/' /boot/config.txt");
+              exec("sudo sed -i -e '/^dtoverlay=pwm/d' /boot/firmware/config.txt");
+              exec("sudo sed -i -e 's/^#dtparam=audio=on/dtparam=audio=on/' /boot/firmware/config.txt");
            }
            break;
 
         case 'DynRDSAdvPIPWMPin':
            if (is_numeric(strpos($myPluginSettings['DynRDSAdvPIPWMPin'], ','))) {
-              exec("sudo sed -i -e 's/^#dtoverlay=pwm/dtoverlay=pwm/' /boot/config.txt");
-              exec("sudo sed -i -e '/^dtoverlay=pwm/c dtoverlay=pwm,pin=" . str_replace(",", ",func=", $myPluginSettings['DynRDSAdvPIPWMPin']) . "' /boot/config.txt");
+              exec("sudo sed -i -e 's/^#dtoverlay=pwm/dtoverlay=pwm/' /boot/firmware/config.txt");
+              exec("sudo sed -i -e '/^dtoverlay=pwm/c dtoverlay=pwm,pin=" . str_replace(",", ",func=", $myPluginSettings['DynRDSAdvPIPWMPin']) . "' /boot/firmware/config.txt");
            } else {
-              exec("sudo sed -i -e 's/^dtoverlay=pwm/#dtoverlay=pwm/' /boot/config.txt");
+              exec("sudo sed -i -e 's/^dtoverlay=pwm/#dtoverlay=pwm/' /boot/firmware/config.txt");
            }
            break;
 
