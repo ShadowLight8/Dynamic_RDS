d8c7749b749371be095d7e5623e8781a9e439ea2
ShadowLight8 <stuff@nickandbeth.com>
2025-11-16 23:12:21 -0500

    Large cleanup in Si4713.py. Getting closer

--- Si4713.py
+++ Si4713.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import os
+from threading import Timer
 from time import sleep
 from datetime import datetime
 from gpiozero import DigitalOutputDevice
@@ -14,8 +15,6 @@ class Si4713(Transmitter):
     logging.info('Initializing Si4713 transmitter')
     super().__init__()
     self.I2C = basicI2C(0x63)  # Si4713 default I2C address
-    self.PS = self.PSBuffer(self, ' ', int(config['DynRDSPSUpdateRate']))
-    self.RT = self.RTBuffer(self, ' ', int(config['DynRDSRTUpdateRate']))
 
   # Si4713 Commands
   CMD_POWER_UP = 0x01
@@ -94,9 +93,6 @@ class Si4713(Transmitter):
     # Verify chip by getting revision
     self._send_command(self.CMD_GET_REV, [], True)
     revData = self.I2C.read(0x00, 9, True)
-    #logging.info(f'Si4713 Part Number: 47{revData[1]:02d}, Firmware: {revData[2]}.{revData[3]}, '
-    #             f'Patch ID: {revData[4]}.{revData[5]}, Component: {revData[6]}.{revData[7]}, '
-    #             f'Chip Revision: {revData[8]}')
     logging.info('Si47%02d - FW %d.%d - Chip Rev %d',
                  revData[1], revData[2], revData[3], revData[8])
     if revData[1] != 13:
@@ -111,20 +107,8 @@ class Si4713(Transmitter):
                   rdsBuffData[5], rdsBuffData[4] + rdsBuffData[5])
     self.totalCircularBuffers = rdsBuffData[2] + rdsBuffData[3]
 
-    # Set reference clock (32.768 kHz crystal)
-    #self._set_property(self.PROP_REFCLK_FREQ, 32768)
-
     # Enable stereo, pilot, and RDS
     self._set_property(self.PROP_TX_COMPONENT_ENABLE, 0x0007)
-
-    # Set audio deviation (68.25 kHz)
-    #self._set_property(self.PROP_TX_AUDIO_DEVIATION, 6825)
-
-    # Set pilot deviation (6.75 kHz)
-    #self._set_property(self.PROP_TX_PILOT_DEVIATION, 675)
-
-    # Set RDS deviation (2 kHz)
-    #self._set_property(self.PROP_TX_RDS_DEVIATION, 200)
 
     # Set pre-emphasis
     if config['DynRDSPreemphasis'] == "50us":
@@ -134,8 +118,13 @@ class Si4713(Transmitter):
 
     # Configure RDS
     self._set_property(self.PROP_TX_RDS_PS_MIX, 0x05)  # Mix mode
-    #self._set_property(self.PROP_TX_RDS_PS_MISC, 0x1808)  # Standard settings
-    self._set_property(self.PROP_TX_RDS_PS_REPEAT_COUNT, 5)  # Repeat 3 times
+    self._set_property(self.PROP_TX_RDS_PS_REPEAT_COUNT, 9)  # Repeat 3 times
+    # TODO: Timing guidance is needed
+    # MIX @ 5 and REPEAT @ 9 - PS ~4 sec, RT ~5.5 sec
+    # Lowering MIX still speed up RT refresh
+    # Lowering REP will speed up PS, Raising REP will slow down PS
+    # TODO: Decide on bit 11 - 0=FIFO and BUFFER use PTY and TP as when written, 1=Force to be this setting
+    self._set_property(self.PROP_TX_RDS_PS_MISC, 0b0001100000001000 | int(config['DynRDSPty'])<<5)
 
     # Set frequency from config
     tempFreq = int(float(config['DynRDSFrequency']) * 100)  # Convert to 10 kHz units
@@ -159,10 +148,6 @@ class Si4713(Transmitter):
     ]
     self._send_command(self.CMD_TX_TUNE_POWER, args)
     sleep(0.02)
-
-    # Set TX_RDS_PS_MISC
-    # TODO: Decide on bit 11 - 0=FIFO and BUFFER use PTY and TP as when written, 1=Force to be this setting
-    self._set_property(self.PROP_TX_RDS_PS_MISC, 0b0001100000001000 | int(config['DynRDSPty'])<<5)
 
     # Set TX_RDS_PI
     self._set_property(self.PROP_TX_RDS_PI, int(config['DynRDSPICode'], 16))
@@ -213,6 +198,9 @@ class Si4713(Transmitter):
       logging.debug('Si4713 updateRDSData active')
       self._updatePS(PSdata)
       self._updateRT(RTdata)
+      # Initial burst of RT groups to get it displayed quickly
+      self._set_property(self.PROP_TX_RDS_PS_MIX, 0x01)  # Mix mode
+      Timer(1, lambda: (logging.info('LAMBDA'), self._set_property(self.PROP_TX_RDS_PS_MIX, 0x05))).start()
 
   def _updatePS(self, psText):
     logging.info('Called _updatePS')
@@ -238,7 +226,6 @@ class Si4713(Transmitter):
   def _updateRT(self, rtText):
     logging.info('Called _updateRT')
 
-    #rtText ='012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345'
     rtMaxLength = self.totalCircularBuffers // 3 * 4
 
     logging.error('Abs Max Length: %d', rtMaxLength)
@@ -257,6 +244,7 @@ class Si4713(Transmitter):
       else:
         rtText = rtText + chr(0x0d) * (4 - len(rtText) % 4)
 
+    # TODO: It might be better to stick with 32 char groups only
     logging.debug('Adj RT %d \'%s\'', len(rtText), rtText.replace('\r','<0d>'))
 
     # Empty circular buffer
@@ -272,6 +260,7 @@ class Si4713(Transmitter):
       rtBytes = [0b00000100, 0b00100000, ab_flag<<4 | segmentOffset]
       rtBytes.extend(list(rtText[i:i+4].encode('ascii')))
       logging.info(rtBytes)
+      # TODO: Can add to buffer twice as a way to slow down update speed
       self._send_command(self.CMD_TX_RDS_BUFF, rtBytes)
       rdsBuffData = self.I2C.read(0x00, 6, True)
       logging.debug('Circular Buffer: %d/%d, Fifo Buffer: %d/%d',
@@ -279,165 +268,6 @@ class Si4713(Transmitter):
                     rdsBuffData[5], rdsBuffData[4] + rdsBuffData[5])
       segmentOffset += 1
 
-      # Will block for ~80-90ms for RDS Group to be sent
-      # Check time, if it has been long enough AND a full RT fragment has been sent, move to next fragment
-      # Flip A/B bit, send next group, if last group set full RT sent flag
-      # Need to make sure full RT group has been sent at least once before moving on
-      #if self.currentGroup == 0 and (datetime.now() - self.lastFragmentTime).total_seconds() >= self.delay:
-      #  self.currentFragment = (self.currentFragment + 1) % len(self.fragments)
-      #  self.lastFragmentTime = datetime.now()
-      #  self.ab = not self.ab
-        # Change \r (0x0d) to be [0d] for logging so it is visible in case of debugging
-
-
-      #if len(self.fragments[-1]) < self.frag_size:
-      #  self.fragments[-1] += chr(0x0d)
-
-
-      # TODO: Seems like this could be improved
-      #rdsBytes = [0b1000<<2 | self.pty>>3, (0b00111 & self.pty)<<5 | self.ab<<4 | self.currentGroup]
-      #rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size]))
-      #rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 1]) if len(self.fragments[self.currentFragment]) - self.currentGroup * self.group_size >= 2 else 0x20)
-      #rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 2]) if len(self.fragments[self.currentFragment]) - self.currentGroup * self.group_size >= 3 else 0x20)
-      #rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 3]) if len(self.fragments[self.currentFragment]) - self.currentGroup * self.group_size >= 4 else 0x20)
-
-      #self.outer.transmitRDS(rdsBytes)
-      #self.currentGroup += 1
-      #if self.currentGroup * self.group_size >= len(self.fragments[self.currentFragment]):
-      #  self.currentGroup = 0
-
-
-
-
-
-
-
-
-
   def sendNextRDSGroup(self):
-    # If more advanced mixing of RDS groups is needed, this is where it would occur
     logging.excessive('Si4713 sendNextRDSGroup')
     sleep(0.25)
-    #self.PS.sendNextGroup()
-    #self.RT.sendNextGroup()
-
-  def transmitRDS(self, rdsBytes):
-    """
-    Transmit RDS group using Si4713's TX_RDS_BUFF command
-    rdsBytes: 8-byte array containing the RDS group
-    """
-    logging.excessive("Transmit %s", ' '.join(f"0x{b:02X}" for b in rdsBytes))
-
-    # Si4713 uses CMD_TX_RDS_BUFF to load RDS data
-    # Command format: CMD, status, FIFO count, RDS data (8 bytes)
-    args = [0b00000100]  # Clear interrupt
-    args.extend(rdsBytes)
-
-    success = self._send_command(self.CMD_TX_RDS_BUFF, args)
-    self._send_command(self.CMD_TX_RDS_BUFF, [0, 0, 0, 0, 0, 0, 0], True)
-    rdsBuffData = self.I2C.read(0x00, 6, True)
-    logging.debug('Circular Buffer: %d/%d, Fifo Buffer: %d/%d',
-                  rdsBuffData[3], rdsBuffData[2] + rdsBuffData[3],
-                  rdsBuffData[5], rdsBuffData[4] + rdsBuffData[5])
-
-    if not success:
-      logging.error('Failed to transmit RDS group')
-      # RDS has failed to update, reset the Si4713
-      self.reset()
-      return
-
-    # RDS specifications indicate 87.6ms to send a group
-    sleep(0.087)
-
-  class PSBuffer(Transmitter.RDSBuffer):
-    # Sends RDS type 0B groups - Program Service
-    # Fragment size of 8, Groups send 4 characters at a time
-    def __init__(self, outer, data, delay=4):
-      self.outer = outer
-      super().__init__(data, 8, 4, delay)
-      # Include outer for the common transmitRDS function that both PSBuffer and RTBuffer use
-
-    def updateData(self, data):
-      super().updateData(data)
-
-      if len(self.fragments) > 12:
-        logging.error('Too many PS fragments: %d (max 12)', len(self.fragments))
-        return
-
-      # Adjust last fragment to make all 8 characters long
-      self.fragments[-1] = self.fragments[-1].ljust(self.frag_size)
-      logging.info('PS %s', self.fragments)
-
-      self.outer._set_property(self.outer.PROP_TX_RDS_PS_MESSAGE_COUNT, 3)
-
-      group = 0
-      for fragment in self.fragments:
-        for chunk in range(self.frag_size // self.group_size):
-          start = chunk * self.group_size
-          rdsBytes = [group]
-          rdsBytes.append(ord(fragment[start]))
-          rdsBytes.append(ord(fragment[start + 1]))
-          rdsBytes.append(ord(fragment[start + 2]))
-          rdsBytes.append(ord(fragment[start + 3]))
-          self.outer._send_command(self.outer.CMD_TX_RDS_PS, rdsBytes)
-          group += 1
-
-    def sendNextGroup(self):
-      sleep(0.25)
-      return
-      if self.currentGroup == 0 and (datetime.now() - self.lastFragmentTime).total_seconds() >= self.delay:
-        self.currentFragment = (self.currentFragment + 1) % len(self.fragments)
-        self.lastFragmentTime = datetime.now()
-        logging.debug('Send PS Fragment \'%s\'', self.fragments[self.currentFragment])
-
-      rdsBytes = [self.currentGroup]
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size]))
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 1]))
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 2]))
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 3]))
-
-      self.outer._send_command(self.outer.CMD_TX_RDS_PS, rdsBytes)
-      #self.outer.transmitRDS(rdsBytes)
-      self.currentGroup = (self.currentGroup + 1) % (self.frag_size // self.group_size)
-      #sleep(0.25)
-
-  class RTBuffer(Transmitter.RDSBuffer):
-    # Sends RDS type 2A groups - RadioText
-    # Max fragment size of 64, Groups send 4 characters at a time
-    def __init__(self, outer, data, delay=7):
-      self.ab = 0
-      super().__init__(data, int(config['DynRDSRTSize']), 4, delay)
-      self.outer = outer
-
-    def updateData(self, data):
-      super().updateData(data)
-      # Add 0x0d to end of last fragment to indicate RT is done
-      # TODO: This isn't quite correct - Should put 0x0d where a break is indicated in the rdsStyleText
-      if len(self.fragments[-1]) < self.frag_size:
-        self.fragments[-1] += chr(0x0d)
-      self.ab = not self.ab
-      logging.info('RT %s', self.fragments)
-
-    def sendNextGroup(self):
-      # Will block for ~80-90ms for RDS Group to be sent
-      # Check time, if it has been long enough AND a full RT fragment has been sent, move to next fragment
-      # Flip A/B bit, send next group, if last group set full RT sent flag
-      # Need to make sure full RT group has been sent at least once before moving on
-      if self.currentGroup == 0 and (datetime.now() - self.lastFragmentTime).total_seconds() >= self.delay:
-        self.currentFragment = (self.currentFragment + 1) % len(self.fragments)
-        self.lastFragmentTime = datetime.now()
-        self.ab = not self.ab
-        # Change \r (0x0d) to be [0d] for logging so it is visible in case of debugging
-        logging.debug('Send RT Fragment \'%s\'', self.fragments[self.currentFragment].replace('\r','<0d>'))
-
-      # TODO: Seems like this could be improved
-      rdsBytes = [0b1000<<2 | self.pty>>3, (0b00111 & self.pty)<<5 | self.ab<<4 | self.currentGroup]
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size]))
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 1]) if len(self.fragments[self.currentFragment]) - self.currentGroup * self.group_size >= 2 else 0x20)
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 2]) if len(self.fragments[self.currentFragment]) - self.currentGroup * self.group_size >= 3 else 0x20)
-      rdsBytes.append(ord(self.fragments[self.currentFragment][self.currentGroup * self.group_size + 3]) if len(self.fragments[self.currentFragment]) - self.currentGroup * self.group_size >= 4 else 0x20)
-
-      self.outer.transmitRDS(rdsBytes)
-      self.currentGroup += 1
-      if self.currentGroup * self.group_size >= len(self.fragments[self.currentFragment]):
-        self.currentGroup = 0
